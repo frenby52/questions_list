60465df4573c31a42eb425d9792c1fe101adc681
frenby52 <aakytin@mail.ru>
2026-09-21 22:39:16 +0300

    migrates entry point to typescript and adds provider

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/global.scss';
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts';
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>,
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+}
